d2f9875127ee26aa2f71227e99c8a379eaaea40f
Vayevn <r.farrand27@hotmail.fr>
2016-04-06 11:53:14 +0200

    Classe carte + tableau de valeurs

---
+++ TCG devoir/Assets/cartesExistantes.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cartesExistantes : MonoBehaviour {
+
+
+	public rapportCoutY[] _rapportCoutY = new rapportCoutY[4];
+	
+}
+
+[System.Serializable]
+public class rapportCoutY
+{
+	public int[] rapportX = new int[3];
+}
+
