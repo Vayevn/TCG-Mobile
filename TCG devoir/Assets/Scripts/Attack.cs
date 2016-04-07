99a02fb27aebb23f20006779bb6e0dda7acca1c2
Etudiant <Etudiant>
2016-04-07 12:50:56 +0200

    Traitement de l'attaque Problème : "tressaillement" au contact >> à trouver : smoother le follow/attaque

---
+++ TCG devoir/Assets/Scripts/Attack.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+	protected Animator animator;
+	AnimatorStateInfo stateInfo;
+
+	public GameObject target;
+
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	void OnCollisionEnter (Collision other) {
+		target = other.gameObject;
+		animator.SetBool ("onContact", true);
+	}
+
+	void OnCollisionExit (Collision other) {
+		animator.SetBool ("onContact", false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (animator) {
+			stateInfo = animator.GetCurrentAnimatorStateInfo (0);
+
+			if (stateInfo.fullPathHash == Animator.StringToHash ("Base Layer.Attack")) 
+			{
+				target.GetComponent<Life> ().currentLife -= 2 * Time.deltaTime;
+
+			}
+		}
+	}
+}
---
+++ TCG devoir/Assets/Scripts/Life.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Life : MonoBehaviour {
+
+	public float currentLife;
+	float maxLife = 3;
+
+	void Start () {
+		currentLife = maxLife;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentLife == 0) {
+			Destroy (this.gameObject);
+		}
+		//regeneration permanente
+
+		if (currentLife < maxLife) {
+			currentLife += Time.deltaTime;
+		} else if (currentLife > maxLife)
+			currentLife = maxLife;
+	}
+}
