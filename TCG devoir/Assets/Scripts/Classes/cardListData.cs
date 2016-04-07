a019d17d8da0b2eee77b3d24d4b9a7d04d893f10
Vayevn <r.farrand27@hotmail.fr>
2016-04-07 16:30:51 +0200

    Commit : class Card, Deck, savecardlist

---
+++ TCG devoir/Assets/Scripts/Classes/cardListData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Sauvegarde liste de carte
+
+[Serializable]
+public class cardListData
+{
+	public List<carte> listeCartesExistantesData;
+}
--- TCG devoir/Assets/cartesExistantes.cs
+++ TCG devoir/Assets/cartesExistantes.cs
@@ -1,11 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class cartesExistantes : MonoBehaviour {
-
-
-	public rapportCoutY[] _rapportCoutY = new rapportCoutY[4];
 	
+	//Ces deux variables faut les rendres statiques, puis editables depuis l'editor avec un custom editor
+	public rapportCoutY[] _rapportCoutY = new rapportCoutY[4];
+	public List<carte> listeCartesExistantes = new List<carte>();
+
+	string filePath;
+	BinaryFormatter formatter = new BinaryFormatter();
+	static public cardListData persistantData = new cardListData();
+
+	public void SaveCurrentCardsList(){
+
+		persistantData.listeCartesExistantesData = listeCartesExistantes;
+
+		filePath = Path.Combine(Application.persistentDataPath, "existingCardList.dat");
+
+		FileStream stream = File.OpenWrite(filePath);
+		
+		formatter.Serialize(stream, persistantData);
+		
+
+		Debug.Log("Card List Saved !");
+
+	}
+
+	public void LoadCardsList(){
+
+		try
+		{
+			using (FileStream stream = File.OpenRead(filePath))
+			{
+				//listeCartesExistantes = liste sauvegardee
+				persistantData = formatter.Deserialize(stream) as cardListData;
+				Debug.Log("La liste de cartes a bien été chargée");
+			}
+		}
+		catch (FileNotFoundException)
+		{
+			Debug.Log("No persistant data to load");
+		}
+//		catch (InvalidCastException)
+//		{
+//			Debug.LogWarning("Persistant data class has changed");
+//		}
+	}
+
 }
 
 [System.Serializable]
